d57aa04adb394275dc84ec2d2e75c637123a3952
Aaron Miller <aaron.miller04@gmail.com>
2013-01-21 13:10:51 -0800

    Started writing chrome://dns URL extractor and sorter

---
+++ extract_chrome_dns_urls.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#
+VERSION_STRING='2013-01-21'
+#
+# extract_chrome_dns_urls.py - when given a saved HTML document from
+#   'chrome://dns', extract all the URLs (http and https protocols) that are
+#   present, sort them in DNS (top-level domain then lower levels) order, and
+#   print to standard output.  With the --hosts flag, prepend each line with
+#   '127.0.0.1' and some whitespace for /etc/hosts blocking.  The purpose of this
+#   is to block ads and tracking sites.
