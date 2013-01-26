1db63524ed8e0fa3e8035efc2b97599b04daf139
Aaron Miller <aaron.miller04@gmail.com>
2013-01-26 14:15:08 -0800

    Fix option documentation; print the URLs (oops!)

--- extract_chrome_dns_urls.py
+++ extract_chrome_dns_urls.py
@@ -5,9 +5,10 @@ VERSION_STRING='2013-01-26 14:15'
 # extract_chrome_dns_urls.py - when given a saved HTML document from
 #   'chrome://dns', extract all the URLs (http and https protocols) that are
 #   present, sort them in DNS (top-level domain then lower levels) order, and
-#   print to standard output.  With the --hosts flag, prepend each line with
-#   '127.0.0.1' and some whitespace for /etc/hosts blocking.  The purpose of this
-#   is to block ads and tracking sites.
+#   print to standard output.  With the --hosts flag, strip everything but the
+#   domain, prepend each line with '127.0.0.1' and some whitespace for
+#   /etc/hosts blocking.  The purpose of this is to block ads and tracking
+#   sites.
 
 import sys
 import re
@@ -40,3 +41,7 @@ if __name__ == '__main__':
 
     all_urls = filter(lambda x: re.match(r'^https?://', x), all_data)
     all_urls = list(set(all_urls))
+
+    # TODO: sort all_urls in DNS order
+
+    print '\n'.join(all_urls)
