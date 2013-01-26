e9d7b3a1aa34533c535902e896b3f59117d54b13
Aaron Miller <aaron.miller04@gmail.com>
2013-01-26 14:03:58 -0800

    Extract URLs from stdin (not tested yet; still need to support options)

--- extract_chrome_dns_urls.py
+++ extract_chrome_dns_urls.py
@@ -8,3 +8,35 @@ VERSION_STRING='2013-01-21 13:45'
 #   print to standard output.  With the --hosts flag, prepend each line with
 #   '127.0.0.1' and some whitespace for /etc/hosts blocking.  The purpose of this
 #   is to block ads and tracking sites.
+
+import sys
+import re
+from getopt import getopt, GetoptError
+from HTMLParser import HTMLParser
+
+all_data = []
+
+def handle_data_factory():
+    """Produces a function that can be called to save the data to a list.
+    Returns a tuple of (<handle_data function>, all_data)."""
+    all_data = list()
+    def handle_data(data):
+        all_data.append(data)
+    return (handle_data, all_data)
+
+if __name__ == '__main__':
+    f = sys.stdin
+
+    # TODO: get options
+
+    s = sys.stdin.read()
+
+    # handle_data is a function which appends its argument to the all_data list
+    handle_data, all_data = handle_data_factory()
+
+    parser = HTMLParser()
+    parser.handle_data = handle_data
+    parser.feed(s)
+
+    all_urls = filter(lambda x: re.match(r'^https?://', x), all_data)
+    all_urls = list(set(all_urls))
